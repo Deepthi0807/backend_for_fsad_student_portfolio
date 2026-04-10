d480c98c348bb119f40ea12078cefa0c9e526585
Deepthi0807 <deepthipallapolu@gmail.com>
2026-04-10 08:27:19 +0530

    connected

--- src/main/java/com/portfolio/backend/dto/RegisterRequest.java
+++ src/main/java/com/portfolio/backend/dto/RegisterRequest.java
@@ -1,17 +1,25 @@
 package com.portfolio.backend.dto;
 
 public class RegisterRequest {
-	 private String username;
+
+	    private String name;
+	    private String email;
+	    private String username;
 	    private String password;
 	    private String confirmPassword;
 
 	    public RegisterRequest() {}
 
+	    public String getName() { return name; }
+	    public String getEmail() { return email; }
 	    public String getUsername() { return username; }
 	    public String getPassword() { return password; }
 	    public String getConfirmPassword() { return confirmPassword; }
 
+	    public void setName(String name) { this.name = name; }
+	    public void setEmail(String email) { this.email = email; }
 	    public void setUsername(String username) { this.username = username; }
 	    public void setPassword(String password) { this.password = password; }
 	    public void setConfirmPassword(String confirmPassword) { this.confirmPassword = confirmPassword; }
 }
+
